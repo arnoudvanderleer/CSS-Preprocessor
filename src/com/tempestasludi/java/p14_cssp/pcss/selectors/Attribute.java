6698fb21f621b2a13fc6973eb6db1dea058a413f
TempestasLudi <tempestasludi@gmail.com>
2016-01-30 13:08:42 +0100

    Added attribute selector class.

---
+++ src/com/tempestasludi/java/p14_cssp/pcss/selectors/Attribute.java
@@ -0,0 +1,28 @@
+package com.tempestasludi.java.p14_cssp.pcss.selectors;
+
+/**
+ * Attribute represents a CSS attribute selector.
+ *
+ * @author Tempestas Ludi
+ */
+public class Attribute extends Selector {
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param name
+	 */
+	public Attribute(String name) {
+		super(name);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
